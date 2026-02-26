258580e84b5739f01841f931a5bf0e41beb6e884
Tomovic <jovantomovic5@gmail.com>
2026-02-26 14:50:17 +0100

    Install and configure Prisma

---
+++ server/src/lib/db.ts
@@ -0,0 +1,11 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
