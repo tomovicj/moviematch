c545be46fd8967e6371178553dcc74dc6c3791d1
Tomovic <jovantomovic5@gmail.com>
2026-02-26 14:15:36 +0100

    server init

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
