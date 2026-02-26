06bede1a0411e343e5b4c185a20a792c2a7588d4
Tomovic <jovantomovic5@gmail.com>
2026-02-26 15:24:41 +0100

    Add and configure better-auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
