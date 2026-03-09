f068a74b88d2e6ad06ab8dd030a551197cb6e987
Tomovic <jovantomovic5@gmail.com>
2026-03-09 17:16:39 +0100

    Add and configure "Vant" UI library

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// import Vant styles
+import 'vant/lib/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VantResolver } from '@vant/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
