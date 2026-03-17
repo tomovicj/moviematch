57a42f8c7ff51327817602c6a4a263f097b3fa35
Tomovic <jovantomovic5@gmail.com>
2026-03-17 19:23:25 +0100

    Client: Added 'No Matching Movies' text in PartyDetailsTab

--- client/src/components/PartyDetailsTab.vue
+++ client/src/components/PartyDetailsTab.vue
@@ -44,39 +44,48 @@ const goToNextMovie = () => {
 <template>
   <div class="h-full flex flex-col">
     <PartyLeaveButton v-if="party" :party="party" />
-    <div class="flex-1 min-h-0 p-4">
-      <MovieCard :movie="matchingMovies[currentIndex]" />
-    </div>
-    <div class="shrink-0 p-4">
-      <!-- Find Matching Movies Button -->
-      <button
-        v-if="!loadedMovies"
-        class="h-10 w-full bg-transparent border border-blue-500 !text-blue-500 font-medium rounded hover:bg-blue-50 active:bg-blue-100 transition-colors cursor-pointer"
-        @click="handleFindMatchingMovies"
-        :disabled="isPending"
-      >
-        <van-loading v-if="isPending" type="spinner" size="24px" vertical>Loading...</van-loading>
-        <span v-else>Find Matching Movies</span>
-      </button>
 
-      <!-- Change Movie Buttons -->
-      <div v-else class="flex gap-4">
+    <!-- No Matching Movies -->
+    <div v-if="loadedMovies && matchingMovies.length === 0" class="text-center text-gray-500 mt-5">
+      No matching movies found
+    </div>
+
+    <div v-else class="flex flex-col flex-1">
+      <!-- Movie Card -->
+      <div class="flex-1 min-h-0 p-4">
+        <MovieCard :movie="matchingMovies[currentIndex]" />
+      </div>
+      <div class="shrink-0 p-4">
+        <!-- Find Matching Movies Button -->
         <button
-          @click="goToPreviousMovie"
-          :disabled="currentIndex === 0"
-          aria-label="Previous movie"
-          class="w-full h-10 w-full bg-transparent border border-blue-500 !text-blue-500 font-medium rounded hover:bg-blue-50 active:bg-blue-100 transition-colors cursor-pointer"
+          v-if="!loadedMovies"
+          class="h-10 w-full bg-transparent border border-blue-500 !text-blue-500 font-medium rounded hover:bg-blue-50 active:bg-blue-100 transition-colors cursor-pointer"
+          @click="handleFindMatchingMovies"
+          :disabled="isPending"
         >
-          <van-icon name="arrow-left" />
+          <van-loading v-if="isPending" type="spinner" size="24px" vertical>Loading...</van-loading>
+          <span v-else>Find Matching Movies</span>
         </button>
-        <button
-          @click="goToNextMovie"
-          :disabled="currentIndex === matchingMovies.length - 1"
-          aria-label="Next movie"
-          class="w-full h-10 w-full bg-transparent border border-blue-500 !text-blue-500 font-medium rounded hover:bg-blue-50 active:bg-blue-100 transition-colors cursor-pointer"
-        >
-          <van-icon name="arrow" />
-        </button>
+
+        <!-- Change Movie Buttons -->
+        <div v-else class="flex gap-4">
+          <button
+            @click="goToPreviousMovie"
+            :disabled="currentIndex === 0"
+            aria-label="Previous movie"
+            class="w-full h-10 w-full bg-transparent border border-blue-500 !text-blue-500 font-medium rounded hover:bg-blue-50 active:bg-blue-100 transition-colors cursor-pointer"
+          >
+            <van-icon name="arrow-left" />
+          </button>
+          <button
+            @click="goToNextMovie"
+            :disabled="currentIndex === matchingMovies.length - 1"
+            aria-label="Next movie"
+            class="w-full h-10 w-full bg-transparent border border-blue-500 !text-blue-500 font-medium rounded hover:bg-blue-50 active:bg-blue-100 transition-colors cursor-pointer"
+          >
+            <van-icon name="arrow" />
+          </button>
+        </div>
       </div>
     </div>
   </div>
